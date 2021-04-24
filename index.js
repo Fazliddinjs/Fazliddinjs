2404296bef14d76d600861c0779eb6c30dcabdb2
Fazliddin <suvonovfazliddin20@gmail.com>
2021-04-24 10:15:49 +0500

    Bu bizning birinchi ishimiz

---
+++ index.js
@@ -0,0 +1,25 @@
+// const obj = {
+//     name: "Fazliddin"
+// }
+// console.log(obj.name);
+
+// let switcher = null;
+// if(switcher){
+//     console.log("Working")
+// }
+
+// switcher = 1;
+// if(switcher){
+//     console.log("Working")
+// };
+
+// console.log(typeof(Boolean('5')));
+ 
+// console.log(typeof(!! '5'))
+// alert("Hello World")
+// let answer = confirm("Are you here");
+// console.log(answer);
+
+//  let answer = prompt("yoshingiz 18 dami?", "ha");
+//  console.log(typeof(answer));
+
